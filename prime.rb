00e8790b3b3edeeaa5e3d0ecd87e4bd045bc28c5
Paula Tulis <tulis.paula@gmail.com>
2019-04-10 17:34:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 def prime?(number)
-   false if !number.integer? || number < 2 
+   false if !number.integer?
+   false if number < 2 
    true if number == 2 
   (2..number-1).each {|x| false if number % x == 0}
    true 
