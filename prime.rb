0ea6bb481421bb5b89d49650159d74e23521df0f
Paula Tulis <tulis.paula@gmail.com>
2019-04-10 17:36:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+
+
+def prime? (number)
+   return false if !number.integer?|| number < 2 
+  return true if number == 2 
+  (2..number - 1).each {|x| return false if number % x == 0}
+   true 
+end
