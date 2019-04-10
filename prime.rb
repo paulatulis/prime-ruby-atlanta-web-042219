d03e5416df0b8f4b36f574cabdf7d5d17ee6ec2c
Paula Tulis <tulis.paula@gmail.com>
2019-04-10 17:32:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(array)
-  if !x.integer? || x < 2 
-      false 
+   false if !x.integer? || x < 2 
+     
     elsif x == 2
     true 
   else (2..array-1).each {|x| false if array % x == 0}
